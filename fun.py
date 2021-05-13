df72e308e0e80cde2a110bd6ff634b8141e4893e
CrappyDill <mddillon47@gmail.com>
2021-05-12 19:59:59 -0700

    first commit

---
+++ fun.py
@@ -0,0 +1,20 @@
+# num = 0
+
+# while num <= 100:
+#     print(num)
+#     num += 10
+
+# print("ALL DONE")
+
+
+# target = 37
+# guess = None
+
+# while guess != target:
+#     guess = input("Please enter a guess..")
+#     if guess == "q" or guess == "quit":
+#         break
+#     guess = int(guess)
+
+# print("alldone")
+
