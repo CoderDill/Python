540dadd709a3484f50a35b79e29c4273f936d64f
CrappyDill <mddillon47@gmail.com>
2021-05-15 22:33:50 -0700

    single_letter_count done

--- python-syntax/python-ds-practice/python-ds-practice/06_single_letter_count/single_letter_count.py
+++ python-syntax/python-ds-practice/python-ds-practice/06_single_letter_count/single_letter_count.py
@@ -1,12 +1,25 @@
 def single_letter_count(word, letter):
     """How many times does letter appear in word (case-insensitively)?
-    
+
         >>> single_letter_count('Hello World', 'h')
         1
-        
+
         >>> single_letter_count('Hello World', 'z')
         0
-        
+
         >>> single_letter_count("Hello World", 'l')
         3
     """
+#Doesn't work and don't know why.
+    # count = 0
+    # for char in word:
+    #     char.lower()
+    #     if char == letter:
+    #         count = count + 1
+    # print(count)
+
+#Does work and do know why.
+    print(word.lower().count(letter.lower()))
+
+
+single_letter_count("I hopepppp this works", "i")
