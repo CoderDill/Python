0a87cf9d22cbce03a3e25707bc7b241671556ac1
CrappyDill <mddillon47@gmail.com>
2021-06-03 12:39:34 -0700

    SpecialWordFinder class done

--- python-oo-practice/python-oo-practice/wordfinder.py
+++ python-oo-practice/python-oo-practice/wordfinder.py
@@ -1,6 +1,24 @@
-"""Word Finder: finds random words from a dictionary."""
+import random
 
 
 class WordFinder:
-    ...
-    def __init__(self):
+    """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """Opens path file and prints how many words were read."""
+        dictionary = open(path)
+        self.words = [w.strip() for w in dictionary]
+        print(f"{len(self.words)} were read")
+
+    def random(self):
+        """Returns a random word"""
+        return (random.choice(self.words))
+
+
+class SpecialWordFinder(WordFinder):
+    """Takes non-words or comments out"""
+
+    def __init__(self, path):
+        dictionary = super().__init__(open(path))
+        self.words = [w.strip() for w in dictionary if w.strip()
+                      and not w.startswith("#")]
