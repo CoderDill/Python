1d88af709b59685352147278333a423b7aa2be0a
CrappyDill <mddillon47@gmail.com>
2021-07-09 14:39:32 -0700

    Go back to here to work on sessions.

--- flask/flask-survey/app.py
+++ flask/flask-survey/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
+from werkzeug.wrappers import response
 from surveys import surveys
 
 # key names will use to store some things in the session;
 # put here as constants so we're guaranteed to be consistent in
 # our spelling of these
-RESPONSES_KEY = "responses"
+CURRENT_SURVEY_KEY = 'current_survey'
+RESPONSES_KEY = 'responses'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cool"
@@ -21,6 +23,29 @@ def survey_home():
 
 @app.route("/", methods=["POST"])
 def pick_survey():
-    survey_id = request.form[""]
-    print(survey_id)
-    return render_template("start_survey.html")
+    survey_id = request.form["survey_code"]
+
+    if request.cookies.get(f"completed_{survey_id}"):
+        return render_template("survey_done.html")
+
+    survey = surveys[survey_id]
+    session[CURRENT_SURVEY_KEY] = survey_id
+
+    return render_template("start_survey.html", survey=survey)
+
+
+@app.route("/start", methods=["POST"])
+def start_survey():
+    session[RESPONSES_KEY] = []
+
+    return redirect("questions/0")
+
+
+@app.route("/questions/0")
+def questions():
+    res = session.get(RESPONSES_KEY)
+    survey_code = session[CURRENT_SURVEY_KEY]
+    survey = surveys[survey_code]
+
+    question = survey.questions[0]
+    return render_template("/question.html", question=question)
