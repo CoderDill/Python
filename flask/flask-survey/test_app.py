1af4c1582f5b82aa361fefa726746fe9d8bed6f4
CrappyDill <mddillon47@gmail.com>
2021-07-11 14:40:23 -0700

    Survey Tests

--- flask/flask-survey/test_app.py
+++ flask/flask-survey/test_app.py
@@ -1,4 +1,6 @@
-from app import app
+from werkzeug.wrappers import response
+from app import app, CURRENT_SURVEY_KEY
+from flask import session
 from unittest import TestCase
 
 
@@ -10,3 +12,34 @@ class ColorViewsTestCase(TestCase):
 
             self.assertEqual(res.status_code, 200)
             self.assertIn("<h3>Welcome</h3>", html)
+
+    def test_pick_survey(self):
+        with app.test_client() as client:
+            res = client.post("/", data={"survey_code": "satisfaction"})
+            html = res.get_data(as_text=True)
+
+            self.assertIn("<h1>Customer Satisfaction Survey</h1>", html)
+
+    def test_start(self):
+        with app.test_client() as client:
+            res = client.post("/start")
+
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(res.location, "http://localhost/questions/0")
+
+    def test_start_followed(self):
+        with app.test_client() as client:
+            res = client.post(
+                "/start", data={"current_survey": "satisfaction"})
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 302)
+            self.assertIn("<h1>Redirecting...</h1>", html)
+
+    def test_session(self):
+        with app.test_client() as client:
+            # with client.session_transaction() as change_seesion: IF YOU WANT TO CHANGE SESSION, THEN TEST
+            # change_seesion[CURRENT_SURVEY_KEY, 'satisfaction']
+            res = client.post("/", data={"survey_code": "satisfaction"})
+
+            self.assertEqual(session[CURRENT_SURVEY_KEY], "satisfaction")
