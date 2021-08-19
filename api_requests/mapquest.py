3c606c21797e91e092c848904756a8016cd67978
CrappyDill <mddillon47@gmail.com>
2021-08-19 15:32:30 -0700

    lat/long routes working

--- api_requests/mapquest.py
+++ api_requests/mapquest.py
@@ -1,7 +1,8 @@
 import requests
 from secrets import API_SECRET_KEY
 
+
 key = API_SECRET_KEY
 
-resp = requests.get("http://www.mapquestapi.com/geocoding/v1/address",
-                    params={'key': key, 'location': 'Sacramento, CA'})
+resp = requests.get("http://www.mapquestapi.com/geocoding/v1",
+                    params={'key': key, 'location': '9415 Frankfurt Ave.'})
