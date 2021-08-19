ef42648816d2cb57114a3eb2299169f9ac0c5794
CrappyDill <mddillon47@gmail.com>
2021-08-18 23:01:44 -0700

    requests post

--- api_requests/itunes.py
+++ api_requests/itunes.py
@@ -1,9 +1,16 @@
 import requests
 
-resp = requests.get(
-    "https://itunes.apple.com/search", params={'term': 'Jack Johnson', 'limit': 5})
+# resp = requests.get(
+#     "https://itunes.apple.com/search", params={'term': 'Jack Johnson', 'limit': 5})
 
-data = resp.json()
+# data = resp.json()
 
-for result in data['results']:
-    print(result['trackName'])
+# for result in data['results']:
+#     print(result['trackName'])
+
+data1 = {
+    'username': 'chicken',
+    'tweets': ['hello', 'goodbye', 'bock bock', {'id': 1, 'text': 'my first tweet'}]
+}
+
+requests.post("https://enfzjd3qh3tfeqs.m.pipedream.net", json=data1)
